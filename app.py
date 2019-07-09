61f627da03f19e1c1f37be8f40dd04f0de7c9092
depretiger <depretiger@gmail.com>
2019-07-09 21:34:45 +0900

    雛形

---
+++ app.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+import pyxel
+
+
+class App:
+    def __init__(self):
+        pass
+    def update(self):
+        pass
+    def draw(self):
+        pass
+
+if __name__ == "__main__":
+    App()
